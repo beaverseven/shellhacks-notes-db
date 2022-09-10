a3994fa02521e4c1a9512b22c1c61b5ddcb657df
butterriolu <derpvin12@gmail.com>
2022-09-09 23:20:11 -0400

    implement notes filter

--- services.js
+++ services.js
@@ -1,52 +1,58 @@
+import _ from "lodash";
+import {
+  onSnapshot,
+  query,
+  where,
+  addDoc,
+} from "firebase/firestore";
 import Notes from "./config.js";
-import { onSnapshot, query, where, addDoc, collection } from "firebase/firestore";
+import { textContains } from "./utils.js";
 
 //implement query match
 export async function handleGet(req, res) {
   const queryParams = req.query; //the user queries
-  console.log(queryParams);
 
-  const field = "school";
-  const comparator = "==";
-  const target = "CUNY Hunter College";
+  function translateQuery(value, key) {
+    return where(key, "==", value);
+  }
 
-  const matchQuery = query(Notes, where(field, comparator, target));
-
-  //const q = query(Notes); <-- select all
+  const whereQueries = _.map(_.omit(queryParams, "notes"), translateQuery);
+  const matchQuery = query(Notes, ...whereQueries);
 
   //fetch from database using query
   onSnapshot(matchQuery, (snapshot) => {
-    const matchedResult = snapshot.docs.map((doc) => ({
+    let matchedResult = snapshot.docs.map((doc) => ({
       ...doc.data(),
       id: doc.id,
     }));
-    //notes contain this and that
-    //notes can be array of strings
+
+    //filter by notes
+    if (queryParams["notes"] != null) {
+      matchedResult = _.filter(matchedResult, (document) => {
+        return textContains(document.notes, queryParams.notes);
+      });
+    }
 
     res.status(200);
-    res.json({ rows: matchedResult });
+    res.json({
+      count: matchedResult.length,
+      rows: matchedResult,
+    });
   });
 }
 
 //implement post to db
 
 export async function AddReq(req, res) {
-
-
   const notesData = req.body;
-  const updatedDoc = { ...notesData, created_at: new Date(), likes: 0, dislikes: 0 } //Updates Document into a new object
-  addDoc(Notes,updatedDoc)
+  const updatedDoc = {
+    ...notesData,
+    created_at: new Date(),
+    likes: 0,
+    dislikes: 0,
+  }; //Updates Document into a new object
+  addDoc(Notes, updatedDoc);
+
   res.status(200);
   res.json(updatedDoc);
-
-
-
-
-
-
-
-
-
 }
-
-
